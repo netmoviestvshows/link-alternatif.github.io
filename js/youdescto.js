c805c8c2cf4914a6766a03744017fd91685b8f1a
netmoviestvshows <100079575+netmoviestvshows@users.noreply.github.com>
2022-10-03 00:47:10 +0700

    create youdescto.js

---
+++ js/youdescto.js
@@ -0,0 +1,230 @@
+function createTags() {
+    var theTags = &quot;&quot;;
+    var theTitle = document.meta_tags.myTitle.value;
+    var theAuthor = document.meta_tags.myAuthor.value;
+    var theDescription = document.meta_tags.myDescription.value;
+    var theKeywords = document.meta_tags.myKeywords.value;
+    var theTahun = document.meta_tags.myTahun.value;
+    var theStadion= document.meta_tags.myStadion.value;
+    var theLive= document.meta_tags.myLive.value;
+    var theHome= document.meta_tags.homePendek.value;
+    var theAway= document.meta_tags.awayPendek.value;
+    var theDay= document.meta_tags.myDay.value;
+    var theUrl= document.meta_tags.myUrl.value;
+   
+  
+  //   Live Streaming Oklahoma Vs City at FULHAM stadium, NCAA Week 5 2022, Full Match
+  
+  
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;Live Stream &quot; + theTitle + &quot; VS &quot;  + theAuthor + &quot; at &quot;+ theStadion +&quot; Stadium | &quot;+ theKeywords +&quot; &quot; + theDescription + &quot; &quot; + theTahun +&quot;\n&quot;;
+    }
+    
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;Team Match: &quot;  + theTitle + &quot; VS &quot;  + theAuthor + &quot; (&quot;   + theHome + &quot; @ &quot;  + theAway +  &quot;)&quot; +&quot;\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;Game/Date: &quot; + theKeywords + &quot; &quot; + theDescription + &quot;, &quot;  + theDay + &quot;, &quot;  + theTahun +   &quot;\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;Stadium/Venue: &quot;  + theStadion +&quot; Stadium &quot; + &quot;\n\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot; + theLive + &quot;: https://www.youtube.com/redirect?q=&quot; + theUrl + &quot;\n\n&quot;;
+    }
+    //   BATAS
+  
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;==================================================================================&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theTitle + &quot; VS &quot;  + theAuthor + &quot; Live Streaming&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theTitle + &quot; VS &quot;  + theAuthor + &quot; Live Broadcast&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theTitle + &quot; VS &quot;  + theAuthor + &quot; Live Match&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theTitle + &quot; VS &quot;  + theAuthor + &quot; Live Today&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theTitle + &quot; VS &quot;  + theAuthor + &quot; Live Now&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theTitle + &quot; VS &quot;  + theAuthor + &quot; Match Day&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theTitle + &quot; VS &quot;  + theAuthor + &quot; Full Game&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theTitle + &quot; VS &quot;  + theAuthor + &quot; Full Match&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theTitle + &quot; VS &quot;  + theAuthor + &quot; En Vivo&quot; + &quot;.\n\n&quot;;
+    }
+  //   BATAS
+  
+  
+      if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAuthor + &quot; VS &quot;  + theTitle + &quot; Live Streaming&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAuthor + &quot; VS &quot;  + theTitle + &quot; Live Broadcast&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAuthor + &quot; VS &quot;  + theTitle + &quot; Live Match&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAuthor + &quot; VS &quot;  + theTitle + &quot; Live Today&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAuthor + &quot; VS &quot;  + theTitle + &quot; Live Now&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAuthor + &quot; VS &quot;  + theTitle + &quot; Match Day&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAuthor + &quot; VS &quot;  + theTitle + &quot; Full Game&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAuthor + &quot; VS &quot;  + theTitle + &quot; Full Match&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAuthor + &quot; VS &quot;  + theTitle + &quot; En Vivo&quot; + &quot;.\n\n&quot;;
+    }
+    //   BATAS
+  
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theHome + &quot; VS &quot;  + theAway + &quot; Live Streaming&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theHome + &quot; VS &quot;  + theAway + &quot; Live Broadcast&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theHome + &quot; VS &quot;  + theAway + &quot; En Vivo&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAway + &quot; VS &quot;  + theHome + &quot; Live Streaming&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAway + &quot; VS &quot;  + theHome + &quot; Live Broadcast&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAway + &quot; VS &quot;  + theHome + &quot; En Vivo&quot; + &quot;.\n\n&quot;;
+    }
+  
+    //   BATAS
+    
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theHome + &quot; VS &quot;  + theAway + &quot; Live&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theHome + &quot; VS &quot;  + theAway + &quot; Today&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theHome + &quot; VS &quot;  + theAway + &quot; Tickets&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theHome + &quot; VS &quot;  + theAway + &quot; picks&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theHome + &quot; VS &quot;  + theAway + &quot; odds&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theHome + &quot; VS &quot;  + theAway + &quot; &quot; + theTahun + &quot;.\n\n&quot;;
+    }
+  
+    //   BATAS
+    
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAway + &quot; VS &quot;  + theHome + &quot; Live&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAway + &quot; VS &quot;  + theHome + &quot; Today&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAway + &quot; VS &quot;  + theHome + &quot; Tickets&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAway + &quot; VS &quot;  + theHome + &quot; picks&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAway + &quot; VS &quot;  + theHome + &quot; odds&quot; + &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;  + theAway + &quot; VS &quot;  + theHome + &quot; &quot; + theTahun + &quot;.\n\n&quot;;
+    }
+  
+  
+    //   BATAS
+  
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;+ theTahun + &quot; &quot; + theKeywords + &quot; Game &quot; + theDescription + &quot;&quot;  +   &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;+ theTahun + &quot; &quot; + theKeywords + &quot; Game &quot; + theDescription + &quot; Live Stream&quot;  +   &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;+ theTahun + &quot; &quot; + theKeywords + &quot; Game &quot; + theDescription + &quot; Full Game&quot;  +   &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;+ theTahun + &quot; &quot; + theKeywords + &quot; Game &quot; + theDescription + &quot; Match Day&quot;  +   &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;+ theTahun + &quot; &quot; + theKeywords + &quot; Game &quot; + theDescription + &quot; Derby Match&quot;  +   &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;+ theTahun + &quot; &quot; + theKeywords + &quot; Game &quot; + theDescription + &quot; Big Game&quot;  +   &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;+ theTahun + &quot; &quot; + theKeywords + &quot; Game &quot; + theDescription + &quot; Big Match&quot;  +   &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;+ theTahun + &quot; &quot; + theKeywords + &quot; Game &quot; + theDescription + &quot; Rival Team&quot;  +   &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;+ theTahun + &quot; &quot; + theKeywords + &quot; Game &quot; + theDescription + &quot; on Facebook&quot;  +   &quot;.\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;&quot;+ theTahun + &quot; &quot; + theKeywords + &quot; Game &quot; + theDescription + &quot; on Youtube&quot;  +   &quot;.\n&quot;;
+    }
+  
+    //   BATAS
+  
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;#&quot; + theKeywords + &quot; #&quot; + theHome + &quot; #&quot;  + theAway + &quot; #&quot;   + theTahun +&quot;\n\n&quot;;
+    }
+     //   BATAS
+  
+     if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;Get unlimited access to all of our video content Become a subscriber to watch your favorite &quot; + theKeywords +  &quot; games from across the country Live and On Demand on any device&quot;  +   &quot;.\n\n&quot;;
+    }
+  
+    //   BATAS
+  
+  
+  if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;Supports by: #ESPN #FOX #NBCSports and&quot; + &quot;\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;#CBS_Broadcasting Inc&quot; + &quot;.\n\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;Video Replay Available After Game Ends&quot; + &quot;.\n&quot;;
+    }
+  
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;Thank you for watching, don&#39;t forget&quot; + &quot;,\n&quot;;
+    }
+    if (!(theTitle == &quot;&quot;)) {
+      theTags += &quot;Subscribe, Like And Share&quot; + &quot;.\n\n&quot;;
+    }
+    document.meta_tags.myTags.value = theTags;
+  }
+  // --&gt;
+  jQuery(&#39;button&#39;).on(&#39;click&#39;, function() {
+    document.querySelector(&quot;#meta-tags&quot;).select();
+    document.execCommand(&#39;copy&#39;);
+  });
