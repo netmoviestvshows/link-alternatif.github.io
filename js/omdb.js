613412b4730c447eb79e211c8cf8b04657a7c151
netmoviestvshows <100079575+netmoviestvshows@users.noreply.github.com>
2022-10-28 03:30:40 +0700

    Update omdb.js

--- js/omdb.js
+++ js/omdb.js
@@ -80,3 +80,56 @@ background: url(${data.Poster});}
 //Call the getMovie() on button click and window load
 searchBtn.addEventListener("click", getMovie);
 window.addEventListener("load", getMovie);
+
+
+
+//TMDB SCRIPT
+
+
+angular.module('movieFinder', [])
+.controller('movieCtrl', function($scope, $http){
+  $scope.$watch('search', function() {
+    fetch(); //watches the search input which refreshes every 800ms
+  });
+
+//function called every 800ms to perform AJAX call
+function fetch(){
+
+//the results only return a partial img path so this is added to provide the full url to display the poster... those tricksters!
+var apiKey = '0ceedd539b0a1efa834d0c7318eb6355';
+  var imgPath = "https://image.tmdb.org/t/p/w300/"    
+  var imgback = "https://image.tmdb.org/t/p/w1280/"
+  
+  //defining the search value from the input
+  var search = $("#search").val();
+  console.log()
+  //this query allows users to search by title which is input by the user
+  $http.get('https://api.themoviedb.org/3/search/multi?api_key=' + apiKey + '&query=' + search)
+    .then(function(response){ 
+          
+
+      //title of first movie in results array
+      $scope.title = response.data.results[0].original_title; 
+      console.log($scope.title)
+
+      //synopsis of the movie
+      $scope.overview = response.data.results[0].overview;
+
+      //img path for poster
+      $scope.poster = imgPath + response.data.results[0].poster_path;
+      console.log($scope.poster);
+
+      //the voter average for the movie returned
+      $scope.rating = 'Rating: '+ response.data.results[0].vote_average;
+      //the voter average for the movie returned
+      $scope.backdrops = imgback + response.data.results[0].backdrop_path;
+      console.log($scope.backdrops);
+      $scope.release = 'Release: '+ response.data.results[0].release_date;
+      $scope.genres = 'Type: '+ response.data.results[0].media_type;
+      // $scope.name = 'Type: '+ response.data.results[0].origin_country;
+        $scope.runtime = 'runtime: ' + response.data.results[0].runtime;
+
+    });
+}
+})
+
