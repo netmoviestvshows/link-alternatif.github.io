1443d87e27b616622e769baa7a0ba2eb3d4ce38f
netmoviestvshows <100079575+netmoviestvshows@users.noreply.github.com>
2022-11-08 15:11:00 +0700

    Create get-title-movie.js

---
+++ js/get-title-movie.js
@@ -0,0 +1,17 @@
+var url = window.location.toString();
+//ambil bagian parameternya
+url.match(/\?(.+)$/);
+var params = RegExp.$1;
+// pisahkan parameter URL ke associative array
+var params = params.split("&");
+var queryStringList = {};
+for(var i=0;i<params.length;i++)
+{   var tmp = params[i].split("=");
+    queryStringList[tmp[0]] = unescape(tmp[1]);
+}
+
+// tampilkan isi associative array
+for(var i in queryStringList)
+{   var res = queryStringList[i].replace(/[+]/g, " ");
+    document.write(i+"  "+res+"<br/><br/>");
+}
