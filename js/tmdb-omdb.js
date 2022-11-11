3640f1958f0c5fe9792dd9f61f391827da8d9a6a
netmoviestvshows <100079575+netmoviestvshows@users.noreply.github.com>
2022-11-11 15:14:00 +0700

    Update tmdb-omdb.js

--- js/tmdb-omdb.js
+++ js/tmdb-omdb.js
@@ -19,18 +19,8 @@ let getMovie = () => {
       .then((data) => {
         //If movie exists in database
         if (data.Response == "True") {
-          result.innerHTML = `
-          <div class="row">
-<div class="col-md-9 text-center">
-<div style="text-align: start;">${data.Title} ${data.Year}  ${data.Plot}</div>
- <div class="genre">
- <div class="rating"><img src="https://api.iconify.design/ep/star-filled.svg?color=yellow&width=10&height=10"><a>${data.imdbRating}</a></div> 
-<div>${data.Genre.split(",").join("</div><div>")}</div>
-</div>   
+          result.innerHTML = `  
 ${data.Actors}
-  
-</div>
-</div>
           `;
         }
         //If movie does NOT exists in database
