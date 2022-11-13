322f37f011b47beaf66849e3e502f0b842b9d8c5
netmoviestvshows <100079575+netmoviestvshows@users.noreply.github.com>
2022-11-13 22:56:02 +0700

    Create get-title-3.js

---
+++ js/get-title-3.js
@@ -0,0 +1,37 @@
+function getQueryStringParam(param) {
+  var url = window.location.toString();
+  url.match(/\?(.+)$/);
+  var params = RegExp.$1;
+  params = params.split("&");
+  var queryStringList = {};
+  for(var i = 0; i < params.length; i++) {
+    var tmp = params[i].split("=");
+    queryStringList[tmp[0]] = unescape(tmp[1]);
+  }
+  return queryStringList[param];
+}
+
+
+
+if (getQueryStringParam("episode")) {
+  var episode = getQueryStringParam("episode");
+  document.getElementById("episode-title").innerHTML = " episode " + episode + "";
+} else {
+  document.getElementById("episode-title").innerHTML = "";
+}
+
+
+if (getQueryStringParam("episode")) {
+  var episode = getQueryStringParam("episode");
+  document.getElementById("episode2-title").innerHTML = " episode " + episode + "";
+} else {
+  document.getElementById("episode2-title").innerHTML = "";
+}
+
+
+if (getQueryStringParam("episode")) {
+  var episode = getQueryStringParam("episode");
+  document.getElementById("episode3-title").innerHTML = " episode " + episode + "";
+} else {
+  document.getElementById("episode3-title").innerHTML = "";
+}
