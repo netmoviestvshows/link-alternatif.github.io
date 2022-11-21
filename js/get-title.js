02bf5c61516cb1cac6762ace07508951a3873bbb
netmoviestvshows <100079575+netmoviestvshows@users.noreply.github.com>
2022-11-22 03:21:18 +0700

    Update get-title.js

--- js/get-title.js
+++ js/get-title.js
@@ -12,6 +12,6 @@ for(var i=0;i<params.length;i++)
 
 // tampilkan isi associative array
 for(var i in queryStringList)
-{   var res = queryStringList[i].replace(/[+]/g, " ");
+{   var res = queryStringList[i].replace(/[-]/g, " ");
     document.write(i+"  "+res+"<br/><br/>");
 }
