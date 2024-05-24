38cfe14003ba1efe2f8500320ef1c513e75805d4
netmoviestvshows <100079575+netmoviestvshows@users.noreply.github.com>
2024-05-25 02:47:14 +0700

    Update url.ku.js

--- js/url.ku.js
+++ js/url.ku.js
@@ -453,18 +453,18 @@ function masterpoker88ld4() {
 } // load 4
 
 //******************************* KDSLOT777 ***************************************/
-var urlkdslot ='https://duniakade.com/?ref=admin02';
+var urlkdslot ='https://kdtoken.vip/?ref=admin02';
 
 function kdslot() {
-    window.open("https://duniakade.com/?ref=admin02");
+    window.open("https://kdtoken.vip/?ref=admin02");
 } // login & Link 1
 
 function kdslot1() {
-    window.location.href ="https://candikds.com/?ref=admin02";
+    window.location.href ="https://kdtoken.vip/?ref=admin02";
 } // link daftar
 
 function kdslot2() {
-    window.open("https://kadegame.com/?ref=admin02");
+    window.open("https://candikds.com/?ref=admin02");
 } // link 2
 
 function kdslot3() {
@@ -472,23 +472,23 @@ function kdslot3() {
 } // link 3
 
 function kdslotld0() {
-    setTimeout(function () { window.location.href = "https://duniakade.com/?ref=admin02"; }, 100);
+    setTimeout(function () { window.location.href = "https://kdtoken.vip/?ref=admin02"; }, 100);
 } // load 0
 
 function kdslotld() {
-    setTimeout(function () { window.location.href = "https://duniakade.com/?ref=admin02"; }, 40000);
+    setTimeout(function () { window.location.href = "https://kdtoken.vip/?ref=admin02"; }, 40000);
 } // load 1
 
 function kdslotld2() {
-    setTimeout(function () { window.location.href = "https://duniakade.com/?ref=admin02"; }, 32000);
+    setTimeout(function () { window.location.href = "https://kdtoken.vip/?ref=admin02"; }, 32000);
 } // load 2
 
 function kdslotld3() {
-    setTimeout(function () { window.location.href = "https://duniakade.com/?ref=admin02"; }, 4000);
+    setTimeout(function () { window.location.href = "https://kdtoken.vip/?ref=admin02"; }, 4000);
 } // load 3
 
 function kdslotld4() {
-    setTimeout(function () { window.location.href = "https://duniakade.com/?ref=admin02"; }, 13000);
+    setTimeout(function () { window.location.href = "https://kdtoken.vip/?ref=admin02"; }, 13000);
 } // load 4
 
 //******************************* MIYABIPOKER ***************************************/
