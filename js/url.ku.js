63e85045fce96d8a6227e79fe647e616f857902c
netmoviestvshows <100079575+netmoviestvshows@users.noreply.github.com>
2024-09-27 00:46:00 +0700

    Update url.ku.js

--- js/url.ku.js
+++ js/url.ku.js
@@ -453,14 +453,14 @@ function masterpoker88ld4() {
 } // load 4
 
 //******************************* KDSLOT777 ***************************************/
-var urlkdslot ='enakkdslot.site/?ref=admin02';
+var urlkdslot ='https://enakkdslot.site/?ref=admin02';
 
 function kdslot() {
-    window.open("enakkdslot.site/?ref=admin02");
+    window.open("https://enakkdslot.site/?ref=admin02");
 } // login & Link 1
 
 function kdslot1() {
-    window.location.href ="enakkdslot.site/?ref=admin02";
+    window.location.href ="https://enakkdslot.site/?ref=admin02";
 } // link daftar
 
 function kdslot2() {
@@ -472,19 +472,19 @@ function kdslot3() {
 } // link 3
 
 function kdslotld0() {
-    setTimeout(function () { window.location.href = "enakkdslot.site/?ref=admin02"; }, 100);
+    setTimeout(function () { window.location.href = "https://enakkdslot.site/?ref=admin02"; }, 100);
 } // load 0
 
 function kdslotld() {
-    setTimeout(function () { window.location.href = "enakkdslot.site/?ref=admin02"; }, 40000);
+    setTimeout(function () { window.location.href = "https://enakkdslot.site/?ref=admin02"; }, 40000);
 } // load 1
 
 function kdslotld2() {
-    setTimeout(function () { window.location.href = "enakkdslot.site/?ref=admin02"; }, 32000);
+    setTimeout(function () { window.location.href = "https://enakkdslot.site/?ref=admin02"; }, 32000);
 } // load 2
 
 function kdslotld3() {
-    setTimeout(function () { window.location.href = "enakkdslot.site/?ref=admin02"; }, 4000);
+    setTimeout(function () { window.location.href = "https://enakkdslot.site/?ref=admin02"; }, 4000);
 } // load 3
 
 function kdslotld4() {
