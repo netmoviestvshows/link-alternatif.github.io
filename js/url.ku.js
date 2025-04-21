ab3ff59645ac96f91ea1211481daf0f178fcfbdf
netmoviestvshows <100079575+netmoviestvshows@users.noreply.github.com>
2025-04-21 11:30:15 +0700

    Update url.ku.js

--- js/url.ku.js
+++ js/url.ku.js
@@ -64,42 +64,42 @@ function nagapokerld4() {
 } // load 4
 
 //******************************* DEWAPOKER ***************************************/
-var urldewapoker = 'https://dew4pokerking.xyz/ref/OMTIE69';
+var urldewapoker = 'https://dewapokerjaya.org/ref/OMTIE69';
 
 function dewapoker() {
-    window.open("https://dew4pokerking.xyz/ref/OMTIE69");
+    window.open("https://dewapokerjaya.org/ref/OMTIE69");
 } // login & Link 1
 
 function dewapoker1() {
-    window.location.href = "https://dew4pokerking.xyz/ref/OMTIE69";
+    window.location.href = "https://dewapokerjaya.org/ref/OMTIE69";
 } // link daftar
 
 function dewapoker2() {
-    window.open("https://dew4pokerways.online/register?ref=OMTIE69");
+    window.open("https://d3wapokerslot.net/register?ref=OMTIE69");
 } // link 2
 
 function dewapoker3() {
-    window.open("https://d3w4pokerace.com/ref/OMTIE69");
+    window.open("https://dew4p0kerways.vip/ref/OMTIE69");
 } // link 3
 
 function dewapokerld0() {
-    setTimeout(function () { window.location.href = "https://dew4pokerking.xyz/ref/OMTIE69"; }, 100);
+    setTimeout(function () { window.location.href = "https://dewapokerjaya.org/ref/OMTIE69"; }, 100);
 } // load 0
 
 function dewapokerld() {
-    setTimeout(function () { window.location.href = "https://dew4pokerking.xyz/ref/OMTIE69"; }, 40000);
+    setTimeout(function () { window.location.href = "https://dewapokerjaya.org/ref/OMTIE69"; }, 40000);
 } // load 1
 
 function dewapokerld2() {
-    setTimeout(function () { window.location.href = "https://dew4pokerways.online/ref/OMTIE69"; }, 32000);
+    setTimeout(function () { window.location.href = "https://dewapokerjaya.org/ref/OMTIE69"; }, 32000);
 } // load 2
 
 function dewapokerld3() {
-    setTimeout(function () { window.location.href = "https://d3w4pokerace.com/ref/OMTIE69"; }, 4000);
+    setTimeout(function () { window.location.href = "https://dewapokerjaya.org/ref/OMTIE69"; }, 4000);
 } // load 3
 
 function dewapokerld4() {
-    setTimeout(function () { window.location.href = "https://dew4pokerking.xyz/ref/OMTIE69"; }, 13000);
+    setTimeout(function () { window.location.href = "https://dewapokerjaya.org/ref/OMTIE69"; }, 13000);
 } // load 4
 
 //******************************* 99DEWA ***************************************/
